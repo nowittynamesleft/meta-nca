c650da1c9b923e908a29bf24508a387b6eb627b8
Meet Barot <nowittynamesleft@gmail.com>
2023-12-09 13:46:20 -0500

    Attempting to simplify nca_local_rule with no activation, still get RuntimeError gradient computation modified by inplace operation version stuff

--- iris_nn.py
+++ iris_nn.py
@@ -252,49 +252,70 @@ class MetaNCA(nn.Module):
         all_architectures = list(itertools.product(*args))
         return random.sample(all_architectures, n)
 
+    def get_random_matrix_inds(self, param):
+        num_update_elements = int(param.nelement()*self.prop_cells_updated)
+        random_inds = torch.randperm(param.nelement())[:num_update_elements]
+        row_indices = random_inds // param.size(1)
+        col_indices = random_inds % param.size(1)
+        index_tuples = list(zip(row_indices.tolist(), col_indices.tolist()))
+        return index_tuples
 
-    def nca_local_rule(self, param, hidden_states): # does not yet take into account the data. Should it? What is a good way to do that? What about using the activations of the network as it is inputting a databatch?
-        #updates = torch.zeros_like(param).to(self.device)
+    def get_forward_states(self, param, hidden_states, i, j):
+        # given the parameter index, what are the neighbors?
+        if i == 0: # if the back unit is the first one
+            forward = param[i+1:, j].flatten() # index all forward weights besides the
+                                               # current one
+            forward_states = hidden_states[i+1:,j,:]
+        elif i == param.shape[0]: # if the back unit is the last one
+            forward = param[:i, j].flatten() # index all except the last one
+            forward_states = hidden_states[:i,j,:] 
+        else: # and if it's in the middle, concat the everything before and everything after
+            forward = torch.cat((param[:i,j].flatten(), param[i+1:, j].flatten())) # 
+            forward_states = torch.cat((hidden_states[:i,j], hidden_states[i+1:, j]))
+        return forward, forward_states
+
+    def get_backward_states(self, param, hidden_states, i, j):
+        if j == 0:
+            backward = param[i, j+1:].flatten()
+            backward_states = hidden_states[i,j+1:,:]
+        elif j == param.shape[1]:
+            backward = param[i, :j].flatten()
+            backward_states = hidden_states[i,:j,:]
+        else:
+            backward = torch.cat((param[i,:j].flatten(), param[i, j+1:].flatten()))
+            #backward_states = torch.cat((hidden_states[i,:j], hidden_states[i, j+1:]), dim=1)
+            #concatenate backward states to average later
+            backward_states = torch.cat((hidden_states[i,:j], hidden_states[i, j+1:]), dim=0)
+        return backward, backward_states
+
+    def get_neighboring_signals(self, param, hidden_states, i, j):
+        forward, forward_states = self.get_forward_states(param, hidden_states, i, j)
+        backward, backward_states = self.get_backward_states(param, hidden_states, i, j)
+
+        concatted_weights = torch.cat((param[i,j][None], torch.mean(forward)[None], torch.mean(backward)[None]))
+        concatted_states = torch.cat((hidden_states[i,j, :].flatten(), torch.mean(forward_states,
+            dim=0).flatten(), torch.mean(backward_states, dim=0).flatten()))
+
+        concatted = torch.cat((concatted_weights, concatted_states))
+        return concatted
+
+    def nca_local_rule(self, param, hidden_states): 
         torch.autograd.set_detect_anomaly(True)
-        updates = torch.zeros(param.shape[0], param.shape[1], 1+hidden_states.shape[2]).to(self.device)
-        #updates = torch.ones(param.shape[0], param.shape[1], 1+hidden_states.shape[2]).to(self.device)
-        idx_in_units = torch.randperm(param.shape[0])[:int(param.shape[0]*self.prop_cells_updated)][:,None]
-        idx_out_units = torch.randperm(param.shape[1])[:int(param.shape[1]*self.prop_cells_updated)][:,None]
-        #import ipdb; ipdb.set_trace()
-        for i in idx_in_units:
-            for j in idx_out_units:
-                #updates[i,j] = self.local_nn(torch.cat([param[i, :].flatten(), param[:, j].flatten(), self.bias[j]]))
-                '''
-                updates[i,j] = self.local_nn(torch.cat([param[i, :].flatten(), param[:, j].flatten()]))
-                '''
-                if i == 0:
-                    forward = param[i+1:, j].flatten()
-                    forward_states = hidden_states[i+1:,j,:]
-                elif i == param.shape[0]:
-                    forward = param[:i, j].flatten()
-                    forward_states = hidden_states[:i,j,:]
-                else:
-                    forward = torch.cat((param[:i,j].flatten(), param[i+1:, j].flatten()))
-                    forward_states = torch.cat((hidden_states[:i,j], hidden_states[i+1:, j]))
-                if j == 0:
-                    backward = param[i, j+1:].flatten()
-                    backward_states = hidden_states[i,j+1:,:]
-                elif j == param.shape[1]:
-                    backward = param[i, :j].flatten()
-                    backward_states = hidden_states[i,:j,:]
-                else:
-                    backward = torch.cat((param[i,:j].flatten(), param[i, j+1:].flatten()))
-                    backward_states = torch.cat((hidden_states[i,:j], hidden_states[i, j+1:]), dim=1)
-                concatted_weights = torch.cat((param[i,j], torch.mean(forward)[None], torch.mean(backward)[None]))
-                concatted_states = torch.cat((hidden_states[i,j, :].flatten(), torch.mean(forward_states, dim=0).flatten(), torch.mean(backward_states, dim=1).flatten()))
-                concatted = torch.cat((concatted_weights, concatted_states))
-                #import ipdb; ipdb.set_trace()
-                updates[i,j, :] = self.local_nn(concatted)
-        weight_updates = updates[:,:,0] 
-        hidden_state_updates = updates[:,:,1:]
+        update_index_tuples = self.get_random_matrix_inds(param)
+        all_inputs = torch.zeros(len(update_index_tuples), 3 + 3*self.hidden_state_dim).to(self.device)
+        # in order to get forward weights and forward_states, i want to mask
+        for k, (i,j) in enumerate(update_index_tuples):
+            all_inputs[k, :] = self.get_neighboring_signals(param, hidden_states, i, j)
+
+        batchwise_updates = self.local_nn(all_inputs) # calculate all updates at once
+        reshaped_updates = torch.zeros(param.shape[0], param.shape[1], 1 +
+            self.hidden_state_dim).to(self.device)
+        for k, (i,j) in enumerate(update_index_tuples):
+            reshaped_updates[i, j, :] = batchwise_updates[k, :]
+
+        weight_updates = reshaped_updates[:,:,0] 
+        hidden_state_updates = reshaped_updates[:,:,1:]
         return weight_updates, hidden_state_updates
-        #for j in idx_out_units:
-        #    self.bias[j] += self.local_nn(torch.cat([torch.zeros((self.out_units,)), self.weight[:,j].flatten(), self.bias[j]]))
 
     def reset_models(self):
         for model in self.models:
@@ -511,8 +532,8 @@ if __name__ == '__main__':
                 #viz_end = time.time()
                 #print('Time for visualization for current step: ' + str(viz_end - viz_start))
             #print('Reparametrize')
-            net.reparametrize_weights()
             #reparam_net = ReparamModule(net)
+            net.reparametrize_weights()
             #print('Done Reparametrize')
             for (X,y) in train_dataloader:
                 #print("NCA forward")
@@ -524,6 +545,7 @@ if __name__ == '__main__':
                     if epoch > chosen_epoch: # only apply loss after many iterations of the rule
                         loss += criterion(outputs, y)
                         training = False
+
             epoch += 1
         # done training
         if args.visualize:
